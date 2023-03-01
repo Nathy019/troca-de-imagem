74cd81fa6060e205056a5ea7b81e00b6cc65fea6
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-03-01 20:26:46 -0300

    Adicionar arquivos de projeto.

---
+++ troca-de-imagem/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace troca_de_imagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxMostrar = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMostrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMostrar
+            // 
+            this.pictureBoxMostrar.Image = global::troca_de_imagem.Properties.Resources.gateirosdeprimeiraviagem;
+            this.pictureBoxMostrar.Location = new System.Drawing.Point(52, 38);
+            this.pictureBoxMostrar.Name = "pictureBoxMostrar";
+            this.pictureBoxMostrar.Size = new System.Drawing.Size(524, 385);
+            this.pictureBoxMostrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxMostrar.TabIndex = 0;
+            this.pictureBoxMostrar.TabStop = false;
+            this.pictureBoxMostrar.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Imagem VISÍVEL";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(389, 465);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Imagem OCULTA";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 537);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxMostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMostrar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMostrar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
